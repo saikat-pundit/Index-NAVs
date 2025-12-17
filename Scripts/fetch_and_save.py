b3d9403d5991bf297cd634605848084803b3bb95
saikat-pundit <saikatpundit@yandex.com>
2025-12-17 20:54:47 +0530

    Update fetch_and_save.py

--- Scripts/fetch_and_save.py
+++ Scripts/fetch_and_save.py
@@ -16,20 +16,12 @@ def format_value(value, key, index_name):
         if index_name in ["INDIA VIX", "USD/INR", "IND 5Y", "IND 10Y", "IND 30Y"]:
             return f"{float(value):.2f}" if key in ['LTP', 'Chng', 'Previous', 'Yr Hi', 'Yr Lo'] else str(float(value))
         if index_name in ["GIFT-NIFTY", "GOLD", "SILVER"] and key in ['LTP', 'Chng', 'Previous', 'Yr Hi', 'Yr Lo']:
-            val = float(value)
-            if val.is_integer():
-                return str(int(val))
-            else:
-                return str(val)
+            return str(int(float(value))) if '.' in str(value) else str(float(value))
         if key in ['Chng', 'LTP', 'Previous', 'Yr Hi', 'Yr Lo']:
-            val = float(value)
-            if val.is_integer():
-                return str(int(val))
-            else:
-                return str(val)
+            return str(int(float(value))) if '.' in str(value) else str(float(value))
         return str(float(value))
     except: return '-'
-
+        
 index_dict = {}
 for name, symbol in TV_SYMBOLS.items():
     url = f"https://scanner.tradingview.com/symbol?symbol={symbol}&fields=close[1],change_abs,price_52_week_high,price_52_week_low,close,change&no_404=true"
