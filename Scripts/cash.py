7221f63dffb80c2e87d8d78b6e08059d501f506c
saikat-pundit <saikatpundit@yandex.com>
2025-12-17 18:56:19 +0530

    Update cash.py

--- Scripts/cash.py
+++ Scripts/cash.py
@@ -1,6 +1,7 @@
 import requests
 import csv
 import os
+from datetime import datetime
 
 url = "https://oxide.sensibull.com/v1/compute/cache/fii_dii_daily"
 response = requests.get(url)
@@ -12,9 +13,15 @@ with open("Data/Cash.csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerow(["Date", "FII Net Buy/Sell", "DII Net Buy/Sell"])
     
-    for date_str in sorted(data["data"]):
+    sorted_dates = sorted(data["data"], reverse=True)
+    
+    for date_str in sorted_dates:
         day = data["data"][date_str]
         if "cash" in day:
+            date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+            formatted_date = date_obj.strftime("%d %b %y")
+            
             fii_val = int(day["cash"]["fii"]["buy_sell_difference"])
             dii_val = int(day["cash"]["dii"]["buy_sell_difference"])
-            writer.writerow([date_str, f"{fii_val} Cr.", f"{dii_val} Cr."])
+            
+            writer.writerow([formatted_date, f"{fii_val} Cr.", f"{dii_val} Cr."])
